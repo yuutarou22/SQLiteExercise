f5fab8240c3ac76f92b14328b7a3587599cbb858
YutaroM <yutaroapp@gmail.com>
2019-09-15 15:14:26 +0900

    004_DTOの作成

---
+++ app/src/main/java/com/example/sqliteexercise/db/BookDTO.java
@@ -0,0 +1,31 @@
+package com.example.sqliteexercise.db;
+
+public class BookDTO {
+    private int mId;
+    private String mTitle;
+    private String mSubtitle;
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmSubtitle() {
+        return mSubtitle;
+    }
+
+    public void setmSubtitle(String mSubtitle) {
+        this.mSubtitle = mSubtitle;
+    }
+}
